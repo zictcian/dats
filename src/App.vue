14ae2e92776711811b72a69794be606459aa8985
zictcian <183424@ids.upchiapas.edu.mx>
2020-06-14 19:17:05 -0500

    first commit2

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="fb-root"></div>
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="110359084046322"
+  theme_color="#7646ff">
+      </div>
   </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -14,6 +19,20 @@ export default {
     HelloWorld
   }
 }
+window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
 </script>
 
 <style>
